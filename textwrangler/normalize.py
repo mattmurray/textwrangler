7c8ef78b1ce9e21808f2ef287a8c424d04314398
Matt Murray <mattmurray@outlook.com>
2019-08-25 17:35:06 +0100

    tidied up TextNormalizer class

--- textwrangler/normalize.py
+++ textwrangler/normalize.py
@@ -7,20 +7,19 @@ from .patterns import (
     RE_HYPHENATED_WORD,
     QUOTE_TRANSLATION_TABLE
 )
+import re
 from sklearn.base import TransformerMixin
 
 class TextNormalizer(TransformerMixin):
 
-    def __init__(self, case=True, spelling=False, hyphenated_words=True, quotation_marks=True, unicode=True,
+    def __init__(self, case=True, spelling=False, hyphenated_words=True, quotation_marks=True, unicode_characters=True,
                  whitespace=True):
 
-        self.output = []
-        self.text = None
         self.case = case
         self.spelling = spelling
         self.hyphenated_words = hyphenated_words
         self.quotation_marks = quotation_marks
-        self.unicode = unicode
+        self.unicode_characters = unicode_characters
         self.whitespace = whitespace
 
     def _normalize_case(self, text: Text) -> Text:
@@ -30,7 +29,8 @@ class TextNormalizer(TransformerMixin):
         return str(TextBlob(text).correct())
 
     def _normalize_hyphenated_words(self, text: Text) -> Text:
-        return RE_HYPHENATED_WORD.sub(r"\1\2", text)
+        return re.sub(r'([a-zA-Z])-([a-zA-Z])', r'\1 \2', text)
+        # return RE_HYPHENATED_WORD.sub(r"\1\2", text)
 
     def _normalize_quotation_marks(self, text: Text) -> Text:
         return text.translate(QUOTE_TRANSLATION_TABLE)
@@ -46,12 +46,12 @@ class TextNormalizer(TransformerMixin):
 
     def transform(self, text, y=None):
 
-        if type(text) == str:
-            self.text = [text]
-        else:
-            self.text = text
+        output = []
 
-        for item in self.text:
+        if type(text) == str:
+            text = [text]
+
+        for item in text:
             output_text = item
 
             if self.spelling == True:
@@ -66,12 +66,12 @@ class TextNormalizer(TransformerMixin):
             if self.quotation_marks == True:
                 output_text = self._normalize_quotation_marks(output_text)
 
-            if self.unicode == True:
+            if self.unicode_characters == True:
                 output_text = self._normalize_unicode(output_text)
 
             if self.whitespace == True:
                 output_text = self._normalize_whitespace(output_text)
 
-            self.output.append(output_text)
+            output.append(output_text)
 
-        return self.output
+        return output
