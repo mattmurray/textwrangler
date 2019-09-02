8b2cbe9769b03bb3aa71e09496a634d27193fac0
Matt Murray <mattmurray@outlook.com>
2019-09-02 15:49:27 +0100

    added docstring for remover

--- textwrangler/remove.py
+++ textwrangler/remove.py
@@ -3,13 +3,33 @@ import string
 from typing import Text
 from bs4 import BeautifulSoup
 from nltk.corpus import stopwords
-from sklearn.base import TransformerMixin
+from sklearn.base import BaseEstimator, TransformerMixin
 import unicodedata
 from textwrangler import TextNormalizer
 
-class TextRemover(TextNormalizer, TransformerMixin):
+class TextRemover(TextNormalizer, BaseEstimator, TransformerMixin):
 
-    def __init__(self, punctuation=True, accents=True, numbers=False, html=False, stop_words=False):
+    def __init__(self, accents=True, html=False, numbers=False, punctuation=True, stop_words=False):
+
+        '''
+        Parameters
+        ----------
+
+        :param accents : default: True
+        If True, removes all accents from characters. For example, 'Café' -> 'Cafe'.
+
+        :param html : default: False
+        If True, strips HTML tags from the text using BeautifulSoup.
+
+        :param numbers : default: False
+        If True, removes all numerical characters from the string.
+
+        :param punctuation : default: True
+        If True, removes all punctuation characters from the string.
+
+        :param stop_words : default: False
+        If True, removes all stop words from the string.
+        '''
 
         self.punctuation = punctuation
         self.accents = accents
@@ -21,7 +41,6 @@ class TextRemover(TextNormalizer, TransformerMixin):
         return text.translate(str.maketrans({a: ' ' for a in string.punctuation}))
 
     def _accents(self, text: Text) -> Text:
-        # return unidecode.unidecode(text)
         return unicodedata.normalize('NFD', text).encode('ascii', 'ignore').decode("utf-8")
 
     def _numbers(self, text: Text) -> Text:
@@ -39,30 +58,27 @@ class TextRemover(TextNormalizer, TransformerMixin):
     def transform(self, text, y=None):
 
         output = []
-
         if type(text) == str:
             text = [text]
 
         for item in text:
-            output_text = item
 
             if self.punctuation == True:
-                output_text = self._punctuation(output_text)
+                item = self._punctuation(item)
 
             if self.accents == True:
-                output_text = self._accents(output_text)
+                item = self._accents(item)
 
             if self.numbers == True:
-                output_text = self._numbers(output_text)
+                item = self._numbers(item)
 
             if self.html == True:
-                output_text = self._html(output_text)
+                item = self._html(item)
 
             if self.stop_words == True:
-                output_text = self._stop_words(output_text)
+                item = self._stop_words(item)
 
-            output_text = self._normalize_whitespace(output_text)
-
-            output.append(output_text)
+            item = self._normalize_whitespace(item)
+            output.append(item)
 
         return output
